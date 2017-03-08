7a4f198559782c9ff5bad1b0b7e0728293bed7dd
sonishikha <shikha.soni08@outlook.com>
2017-03-08 22:54:57 +0530

    test

--- upload_image.php
+++ upload_image.php
@@ -20,18 +20,25 @@ if (move_uploaded_file($_FILES["image_file"]["tmp_name"], $main_file)) {
     echo "Sorry, there was an error uploading your file.";
 }
 
+$inFile = $main_file;
+$outFile = "test-thumbnail.jpg";
+$image = new Imagick($inFile);
+$image->thumbnailImage(755, 450);
+$image->writeImage($horizontal_file);
+
+/*
 $im = imagecreatefrompng($main_file);
 //$size = min(imagesx($im), imagesy($im));
 $im2 = imagecrop($im, ['x' => 0, 'y' => 0, 'width' => 755, 'height' => 450]);
 if ($im2 !== FALSE) {
     var_dump(imagepng($im2, $_FILES["image_file"]["name"]));
-    /*if(move_uploaded_file(, $horizontal_file)){
+    if(move_uploaded_file(, $horizontal_file)){
         echo "shikha";
     }else{
         echo "bye";
-    }*/
+    }
 }else{
     echo "hiii";
-}
+}*/
 /* } */
 ?>
